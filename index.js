3a282912067ed085d027ed86de2f4d7f1468b729
Nestor Padilla <pcu18021379@gmail.com>
2023-07-17 18:38:35 -0600

    Displaying the boxes

--- index.js
+++ index.js
@@ -24,10 +24,78 @@ dropdowns.forEach(ele => {
             });
             ele.classList.add('active');
             
-            if (selected.innerText == "1") {
-                console.log("pep"); 
+            if (selected.innerText == "Burbuja") {
+                showBuble();
+                console.log("pep");
+                console.log(array); 
+                array = [];
             }
         })
     })
 
 });
+
+
+/**** BUBLE SORTING ****/
+const $visualizer = document.getElementById('visualizer');
+let aux = 0;
+let array = new Array();
+let length = 0;
+
+
+function generateRandomNumbers(length) {
+    for (let i = 0; i < length; i++) {
+        let randomNumber = Number(prompt(`Type the length of the ${i+1} element`));
+        array.push(randomNumber);
+    }
+}
+
+function askLengthArray() {
+    return Number(prompt("Type the length of the array"))
+}
+
+function bubleSort(array){
+    const newArray = [...array];
+    for (let i = 0; i < newArray.length; i++) {
+        for (let j = 0; j < newArray.length; j++) {
+            if (newArray[j] > newArray[j+1]) {
+                aux = newArray[j];
+                newArray[j] = newArray[j+1];
+                newArray[j+1] = aux;
+            }
+        }
+    }
+    return newArray;
+}
+
+async function showBuble(){
+    try {
+        length = await askLengthArray();
+        generateRandomNumbers(length);
+        let sortArray = bubleSort(array);
+        createElement(length, sortArray);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function createElement(length, array) {
+    for (let i = 0; i < length; i++){
+        const newBox = document.createElement('div');
+        const valueBox = document.createElement('p');
+        valueBox.textContent = array[i].toString();
+        valueBox.classList.toggle('font-family');
+        newBox.classList.toggle('box');
+        newBox.appendChild(valueBox);
+        $visualizer.appendChild(newBox);
+    }
+
+}
+
+
+
+
+
+
+
+
