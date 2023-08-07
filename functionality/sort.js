173c2d7434e0484ad89099a27fae21d539732ec8
Nestor Padilla <pcu18021379@gmail.com>
2023-08-06 19:47:25 -0600

    Solve the animation, and only one animation for loop

--- functionality/sort.js
+++ functionality/sort.js
@@ -1,31 +1,67 @@
 const d = document;
 
 const bubleSort = (arrayElements, btnSort) => {
-    let sortedArray = [];
+    let unsortedArray = [];
     d.addEventListener('click', e => {
         if (e.target.matches(btnSort)){
 
             for (let i = 0; i < arrayElements.length; i++) {
-                sortedArray.push(arrayElements[i]);
+                unsortedArray.push(arrayElements[i]);
             }
-            sort(sortedArray);
-            sortedArray = [];
+            sort(unsortedArray);
+            unsortedArray = [];
         }
     })
 }
 
 
 function sort(array) {
-    for (let i = 0; i < array.length - 1; i++) {
-        for (let j = 0; j < array.length - 1; j++) {
-            if (Number(array[j].textContent) > Number(array[j + 1].textContent)) {
-                let swapp = array[j].textContent;
-                array[j].textContent = array[j + 1].textContent
-                array[j + 1].textContent = swapp;
+    let newArray = array;
+    for (let i = 0; i < newArray.length - 1; i++) {
+        for (let j = 0; j < newArray.length - 1; j++) {
+            if (Number(newArray[j].textContent) > Number(newArray[j + 1].textContent)) {
+                let iniPosition = newArray[j].getBoundingClientRect();
+                let maxPosition = newArray[j + 1].getBoundingClientRect();
+
+                
+                
+                
+                let amount = Math.floor(maxPosition.left - iniPosition.left);
+                sortAnimation(amount ,newArray[j], newArray[j + 1]);
+                
+                let swapp = newArray[j];
+                newArray[j] = newArray[j + 1];
+                newArray[j + 1] = swapp;
             }
         }
     }
+    newArray.forEach(element => {
+        console.log(element.innerText);
+    });
 }
+
+
+function sortAnimation(amount, element, greaterElement) {
+
+    let initalPosition = 0;
+    let maxPosition = 0;
+
+
+    greaterElement.style.left = "0px";
+    let interval = setInterval(() => {
+        if (initalPosition === amount) clearInterval(interval);
+        else {
+            initalPosition++;
+            element.style.left = `${initalPosition}px`;
+            
+            maxPosition--;
+            greaterElement.style.left = `${maxPosition}px`;
+        }
+    }, 20)
+}
+
+
+
 
 
 
